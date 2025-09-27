c038592cc4604371330054bea2d1b8def7bbf4a0
gubiczam <gubiczamilan2004@gmail.com>
2025-09-27 12:14:41 +0200

    fix(hello-island): align Vite typings and cast plugin to PluginOption

--- examples/hello-island/vite.config.ts
+++ examples/hello-island/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
+import { defineConfig, type PluginOption } from "vite";
 import zeonPlugin from "@usezeon/vite-plugin";
 
 export default defineConfig({
-  plugins: [zeonPlugin()]
+  plugins: [zeonPlugin() as PluginOption]
 });
